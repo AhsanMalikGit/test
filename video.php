acbb69ecba9617d9c77f3bc14749be0710d9a9c3
Ahsan Malik <ahsan@manyainternational.com>
2017-02-07 17:30:25 +0530

    remove text

--- video.php
+++ video.php
@@ -4,7 +4,7 @@
 <video width="400" controls>
   <source src="Big_Buck_Bunny.mp4" type="video/mp4">
   <source src="mov_bbb.ogg" type="video/ogg">
-  Your browser does not support HTML5 video. Your browser does not support HTML5 video.
+  Your browser does not support HTML5 video.
 </video>
 
 <p>
